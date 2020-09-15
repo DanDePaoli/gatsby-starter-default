6682edaae27cc03cb4f28a48f477eee1c46b0df0
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-09-14 17:29:43 -0700

    Added basic modal on hover functionality

--- src/pages/map.js
+++ src/pages/map.js
@@ -1,9 +1,16 @@
-import React from "react"
-import { GoogleMap, Marker } from "react-google-maps";
+import React, { useState } from "react"
+import { GoogleMap, Marker, InfoWindow } from "react-google-maps";
 
 
 const Map = () => {
 
+  const [ icon, setIcon ] = useState("https://storage.googleapis.com/support-kms-prod/SNP_2752125_en_v0");
+
+  const [ infoWindow, setInfoWindow ] = useState(false);
+
+  const iconChange = () => {
+    setIcon( "https://storage.googleapis.com/support-kms-prod/SNP_2752129_en_v0");
+  }
 
 
   return(
@@ -12,8 +19,19 @@ const Map = () => {
     defaultCenter={{ lat: 37.786840, lng: -122.392240 }}
   >
 <Marker
+icon={{
+  url:icon, scaleSize:new window.google.maps.Size(15, 15)
+}}
+onMouseOver={() => {
+  iconChange()
+setInfoWindow(true)}
+}
 position={{ lat: 37.786840, lng: -122.392240 }}
 />
+{ infoWindow ? <InfoWindow
+position={{ lat: 37.786840, lng: -122.392240 }}>
+  <h1>InfoWindow</h1>
+  </InfoWindow> :null}
   </GoogleMap>
 
   )
