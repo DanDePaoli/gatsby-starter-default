8d90cbc0c05626dd450ba40fb4fd988eb626a7d3
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-09-21 10:27:51 -0700

    Commiting latest for Rob

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Modal from 'react-responsive-modal';
 import Layout from "../components/layout"
 import Map from "./map.js";
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
-
+import * as Data from "./data.json"
 
 // const MyMapComponent = withScriptjs(withGoogleMap((props) =>
 //   <GoogleMap
@@ -22,27 +22,44 @@ const MapComponent = withScriptjs(withGoogleMap(Map));
 export default function IndexPage() {
 
   const [modal, setModal] = useState(false);
+  const [data, setData] = useState(null);
 
   return (
     <Layout>
-      <div style={{ width: '100%', height: '80vh' }} >
+      <div className="flex">
+        <div className="w-1/2 mb-4 ml-2 shadow-lg">
+          <div className="" style={{ width: '100%', height: '100vh' }} >
 
-        <MapComponent
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${"AIzaSyDkgOjcI83-cYqzCZPWfQZhv8sadTCXOZk"}`}
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `100%` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-          setModal={setModal}
-        />
+            <MapComponent
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${"AIzaSyDkgOjcI83-cYqzCZPWfQZhv8sadTCXOZk"}`}
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `100%` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+              setModal={setModal}
+              setData={setData}
+            />
 
+          </div>
+
+          {data ? <Modal
+            open={modal}
+            onClose={() => setModal(false)}>
+            <div className="flex">
+              <div className="w-full">
+
+                <img src={data.properties.Name} style={{ height: `100px`, width: '100px' }} />
+              </div>
+              <div className="w-full ml-3">
+                <h1>{data.properties.NAME}</h1>
+                <p>{data.properties.DESCRIPTION}</p>
+              </div>
+            </div>
+          </Modal>
+
+            : null}
+
+        </div>
       </div>
-
-      <Modal
-        open={modal}
-        onClose={() => setModal(false)}>
-        <h1>Modal</h1>
-        <p>Window</p>
-      </Modal>
 
     </Layout>
   )
--- src/pages/map.js
+++ src/pages/map.js
@@ -1,48 +1,82 @@
-import React, { useState } from "react"
-import { GoogleMap, Marker, InfoWindow } from "react-google-maps";
-import * as Data from "./data.json"
+import React, { useState } from 'react'
+
+import { GoogleMap, Marker, InfoWindow } from 'react-google-maps'
+
+import * as Data from './data.json'
 
 
 const Map = (props) => {
 
-  const [icon, setIcon] = useState("https://storage.googleapis.com/support-kms-prod/SNP_2752125_en_v0");
+  const [icon, setIcon] = useState(null);
+  const [infoWindow, setInfoWindow] = useState(null)
 
-  const [infoWindow, setInfoWindow] = useState(false);
-
-  const iconChange = () => {
-    setIcon("https://storage.googleapis.com/support-kms-prod/SNP_2752129_en_v0");
+  const iconChange = (data) => {
+    if (icon && data.properties.ID == icon.properties.ID) {
+      return "https://storage.googleapis.com/support-kms-prod/SNP_2752129_en_v0"
+    } else {
+      return "https://storage.googleapis.com/support-kms-prod/SNP_2752125_en_v0"
+    }
   }
-
 
   return (
     <GoogleMap
-      defaultZoom={12}
-      defaultCenter={{ lat: 37.786840, lng: -122.392240 }}
+      defaultZoom={15}
+      defaultCenter={{
+        lat: 37.788601,
+        lng: -122.393478
+      }}
     >
+
       {Data.features.map((Data) => (
         <Marker
-        key={Data.properties.ID}
+          key={Data.properties.ID}
           icon={{
-            url: icon, scaleSize: new window.google.maps.Size(15, 15)
+            url: iconChange(Data),
+            scaleSize: new window.google.maps.Size(25, 25)
           }}
           onMouseOver={() => {
-            iconChange()
-            setInfoWindow(true)
-          }
-          }
-          onClick={() => props.setModal(true)}
-          position={{ lat: Data.geometry.coordinates[0], lng: Data.geometry.coordinates[1] }}
+            setIcon(Data)
+            setInfoWindow(Data)
+          }}
+          onClick={() => {
+            props.setmodal(true)
+            props.setData(Data)
+          }}
+          position={{
+            lat: Data.geometry.coordinates[0],
+            lng: Data.geometry.coordinates[1]
+          }}
         />
 
+      )
+      )}
+      {infoWindow ?
+        <InfoWindow
+          key={infoWindow.properties.ID}
+          position={{
+            lat: infoWindow.geometry.coordinates[0]+.001,
+            lng: infoWindow.geometry.coordinates[1]
+          }}
+          onCloseClick={() => setInfoWindow(null)}
+        >
+          <div>
+            <img className="w-15 h-12 mb-2" src={infoWindow.properties.Image} style={{ width: '100px', height: '100px' }} />
+            <h2 className="text-sm mb-3" style={{ fontFamily: "coustard" }}>{infoWindow.properties.NAME}</h2>
+            <p className="text-sm" style={{ fontFamily: 'Gentium Basic' }}>{infoWindow.properties.DESCRIPTION}</p>
+            <p className="text-sm" style={{ fontFamily: 'Gentium Basic' }}>Zestimate: ${infoWindow.properties.Zestimate}M</p>
+            <p className="text-sm" style={{ fontFamily: 'Gentium Basic' }}>{infoWindow.properties.Bedroom} Bds | {infoWindow.properties.Bathroom} Ba | {infoWindow.properties.SquareFeet} sqft</p>
+            <p className="text-sm" style={{ fontFamily: 'Gentium Basic' }}>Pricer Per Foot: ${infoWindow.properties.PricePerFoot}/square foot</p>
 
-      ))}
+            <p className="text-sm" style={{ fontFamily: 'Gentium Basic' }}>Zestimate History {infoWindow.properties.OneYr}% 1Yr | {infoWindow.properties.FiveYr}% 5Yr | {infoWindow.properties.TenYr}% 10Yr</p>
 
-      {infoWindow ? <InfoWindow
-        position={{ lat: 37.786840, lng: -122.392240 }}>
-        <h1>InfoWindow</h1>
-      </InfoWindow> : null}
+            <p className="text-sm" style={{ fontFamily: 'Gentium Basic' }}>Neigborhood Avg {infoWindow.properties.OneYrN}% 1Yr | {infoWindow.properties.FiveYrN}% 5Yr | {infoWindow.properties.TenYrN}% 10Yr</p>
+
+            <p className="text-sm" style={{ fontFamily: 'Gentium Basic' }}>City/Township Avg {infoWindow.properties.OneYrC}% 1Yr | {infoWindow.properties.FiveYrC}% 5Yr | {infoWindow.properties.TenYrC}% 10Yr</p>
+
+          </div>
+        </InfoWindow>
+        : null}
     </GoogleMap>
-
   )
 }
 
